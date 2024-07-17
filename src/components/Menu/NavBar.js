5d403c2ea0e58704d66a84ed55d12348c18118f5
Francisco Ramos <francisco.ramos@gmail.com>
2024-07-17 18:56:25 -0300

    quickFIix

--- src/components/Menu/NavBar.js
+++ src/components/Menu/NavBar.js
@@ -16,7 +16,7 @@ function NavBar() {
     <React.Fragment>   
       <header>
       <nav >      
-        <Link className="navbar-brand me-lg-5" to="/">
+        <Link className="navbar-brand me-lg-5" to="/dashboard">
           <img className="navbar-brand-light" src="img/favicon/favicon.png" alt="CryptoDog" />
            <spam className="appTitle">CryptoSniffer</spam>
         </Link>
@@ -27,18 +27,17 @@ function NavBar() {
 
         {localStorage.getItem("token") && 
           <button onClick={onLogoutClick}>logout</button>}
-        {localStorage.getItem("token") == null&& <Link to="/login">        
+        {localStorage.getItem("token") == null&& 
+        <Link to="/login">        
           <button className="login-button">Login</button>        
         </Link>     }
-        
-        
-        
-        {/* <div className="d-flex align-items-center">
+                
+        <div className="d-flex align-items-center">
           <button className="navbar-toggler d-lg-none collapsed" type="button" data-bs-toggle="collapse"
             data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-        </div> */}
+        </div> 
 
       </nav>  
       </header>         
